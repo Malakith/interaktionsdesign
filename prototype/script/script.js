b8ae8bd0bc4a8b810cfae4650183979b16eb07ff
Malakith <m261087@gmail.com>
2015-03-03 19:17:46 +0100

    First commit

---
+++ prototype/script/script.js
@@ -0,0 +1,53 @@
+function table() {
+    return jQuery('<div/>', {}).addClass("container").draggable({
+        containment: "parent",
+        drag: function( event, ui ) {
+            var snapTolerance = $(this).draggable('option', 'snapTolerance');
+            var topRemainder = ui.position.top % 20;
+            var leftRemainder = ui.position.left % 20;
+
+            if (topRemainder <= snapTolerance) {
+                ui.position.top = ui.position.top - topRemainder;
+            }
+
+            if (leftRemainder <= snapTolerance) {
+                ui.position.left = ui.position.left - leftRemainder;
+            }
+        }
+    });              // The function returns the product of p1 and p2
+};
+
+function settingsButton() {
+    var button = jQuery('<div/>', {}).addClass("settingsButton");
+
+    button.click(function(){
+        $(this).closest(".container").append(options());
+    });
+    return button;
+};
+
+function options() {
+    var options = jQuery('<div/>', {}).addClass("options");
+    var closeButton = jQuery('<div/>', {}).addClass("closeButton");
+    var container = $(this).closest(".container");
+    $(this).height(container.height());
+    $(this).width(container.width());
+    closeButton.click(function(){
+        $(this).closest(".container").fadeOut(200, function() { $(this).remove(); });
+    });
+    options.append(closeButton);
+    return options;
+};
+
+$(document).ready(function() {
+    //First insert
+    $("#insert1").click(function(){
+        //Here we create the stuff we wish to put inside
+        $("#frame").append(table().append(settingsButton()));
+    })
+    //Second insert
+    $("#insert2").click(function(){
+        //Here we create the stuff we wish to put inside
+        $("#frame").append(table().append(settingsButton()));
+    })
+});
